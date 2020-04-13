5143f1a66bbf35821576d908ca725cb079597a0d
jonjonjabon <jonny.knott.jk@gmail.com>
2020-04-13 12:07:46 +0100

    - Added tests for new controller and service - Updated Postman collection

--- test/Glue.Delivery.WebApi.Integration.Test/Infrastructure/ServerFactory.cs
+++ test/Glue.Delivery.WebApi.Integration.Test/Infrastructure/ServerFactory.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections.Generic;
 using Glue.Delivery.Services;
 using Glue.Delivery.WebApi.Integration.Test.Services;
+using Glue.Delivery.WebApi.Services;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
 
 namespace Glue.Delivery.WebApi.Integration.Test.Infrastructure
 {
@@ -29,6 +33,8 @@ namespace Glue.Delivery.WebApi.Integration.Test.Infrastructure
             {
                 //Service replacement
                 services.ReplaceServiceType<IDeliveryService, TestDeliveryService>();
+                services.ReplaceServiceType<IDeliveryStateService, TestDeliveryStateService>();
+                services.RemoveAll(typeof(IHostedService));
             });
         }
     }
